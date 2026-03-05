c40e5a37536187780e65213abc070b44e7393787
PieMenard <pinedo.m.gustavo@gmail.com>
2026-03-05 15:59:38 -0400

    feat: integrate Neon authentication and user context

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,30 @@ import ProfilePage from './pages/ProfilePage';
 import Auth from './pages/Auth';
 import Account from './pages/Account';
 import Navbar from './components/layout/Navbar';
+import { NeonAuthUIProvider } from '@neondatabase/neon-js/auth/react';
+import { authClient } from './lib/auth';
+import AuthProvider from './context/AuthContext';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <div className="min-h-screen flex flex-col">
-                <Navbar />
-                <main className="flex-1">
-                    <Routes>
-                        <Route index element={<Home />} />
-                        <Route path="/onboarding" element={<Onboarding />} />
-                        <Route path="/profile" element={<ProfilePage />} />
-                        <Route path="/auth/:pathname" element={<Auth />} />
-                        <Route path="/account/:pathname" element={<Account />} />
-                    </Routes>
-                </main>
-            </div>
-        </BrowserRouter>
+        <NeonAuthUIProvider emailOTP authClient={authClient} defaultTheme="dark">
+            <AuthProvider>
+                <BrowserRouter>
+                    <div className="min-h-screen flex flex-col">
+                        <Navbar />
+                        <main className="flex-1 pt-16">
+                            <Routes>
+                                <Route index element={<Home />} />
+                                <Route path="/onboarding" element={<Onboarding />} />
+                                <Route path="/profile" element={<ProfilePage />} />
+                                <Route path="/auth/:pathname" element={<Auth />} />
+                                <Route path="/account/:pathname" element={<Account />} />
+                            </Routes>
+                        </main>
+                    </div>
+                </BrowserRouter>
+            </AuthProvider>
+        </NeonAuthUIProvider>
     );
 };
 export default App;
